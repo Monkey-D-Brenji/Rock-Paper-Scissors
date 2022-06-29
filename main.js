46d473f1de88c8b42b06b0d9a7c03e61e202ebde
GodUsopp <an.brendancode@gmail.com>
2022-06-29 15:04:27 -0700

    Fix functions and make score keeper
    added a way to keep score and to display a message based on who
    had more wins at the end of 5 rounds.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let userScore = 0;
+let compScore = 0;
 
 function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -21,8 +23,10 @@ function playRound() {
     (compInput === "paper" && choice === "rock") ||
     (compInput === "scissors" && choice === "paper")
   ) {
+    ++compScore;
     console.log(compInput + " beats " + choice + " you lose");
   } else {
+    ++userScore;
     console.log(choice + " beats " + compInput + " you win");
   }
 }
@@ -30,6 +34,12 @@ function playRound() {
 function game() {
   for (i = 0; i < 5; i++) {
     playRound();
+  }
+
+  if (userScore > compScore) {
+    console.log("You won! Congratulations");
+  } else {
+    console.log("You lost! Try again");
   }
 }
 
