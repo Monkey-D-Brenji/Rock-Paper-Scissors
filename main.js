e0125cf06dfaafe4fba45537f4b0514bd827c9d2
GodUsopp <an.brendancode@gmail.com>
2022-07-05 15:22:22 -0700

    Create button and update functions
    Created buttons and updated javascript file. Added DOM manipulation
    that interacts with buttons and plays rounds until a final winner is
    decided.

--- main.js
+++ main.js
@@ -2,47 +2,63 @@ const choices = ["rock", "paper", "scissors"];
 let userScore = 0;
 let compScore = 0;
 
+const btn = document.querySelectorAll("input");
+btn.forEach((button) => {
+  button.addEventListener("click", function () {
+    playRound(button.value);
+  });
+});
+
+function disableButtons() {
+  btn.forEach((elem) => {
+    elem.disabled = true;
+  });
+}
+
+const div = document.createElement("div");
+document.body.appendChild(div);
+
+const divTwo = document.createElement("div");
+document.body.appendChild(divTwo);
+
 function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function userPlay() {
-  let choice = prompt("Pick Rock, Paper, or Scissors");
-  choice = choice.toLowerCase();
-  return choice;
-}
+function playRound(choice) {
+  let comp = computerPlay();
+  let result = "";
+  let finalScore = "";
 
-function playRound() {
-  let compInput = computerPlay();
-  let choice = userPlay();
-
-  if (choice === compInput) {
-    console.log("tie");
-  } else if (
-    (compInput === "rock" && choice === "scissors") ||
-    (compInput === "paper" && choice === "rock") ||
-    (compInput === "scissors" && choice === "paper")
+  if (
+    (choice === "rock" && comp === "scissors") ||
+    (choice === "paper" && comp === "rock") ||
+    (choice === "scissors" && comp === "paper")
   ) {
-    ++compScore;
-    console.log(compInput + " beats " + choice + " you lose");
+    result = choice + " beats " + comp + " you win!";
+    userScore += 1;
+  } else if (
+    (choice === "rock" && comp === "paper") ||
+    (choice === "paper" && comp === "scissors") ||
+    (choice === "scissors" && comp === "rock")
+  ) {
+    result = comp + " beats " + choice + " you lose!";
+    compScore += 1;
   } else {
-    ++userScore;
-    console.log(choice + " beats " + compInput + " you win");
+    result = "tie!";
   }
+
+  if (userScore === 3) {
+    finalScore = "Congrats You Win!";
+    disableButtons();
+  } else if (compScore === 3) {
+    finalScore = "You Lose! Try again";
+    disableButtons();
+  }
+
+  div.textContent = result;
+  divTwo.textContent = finalScore;
+  return;
 }
 
-function game() {
-  for (i = 0; i < 5; i++) {
-    playRound();
-  }
-
-  if (userScore > compScore) {
-    console.log("You won! Congratulations");
-  } else if (userScore === compScore) {
-    console.log("Tied! Try again");
-  } else {
-    console.log("You lost! Try again");
-  }
-}
-
-game();
+console.log(btn);
